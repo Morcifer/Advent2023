31cddb9e3cfe7cc7162cac5bb1bbe227937482b5
Morcifer <mor.verbin@coolblue.nl>
2023-11-23 11:07:23 +0100

    Initial commit.

---
+++ AdventOfCode.Tests/TestDay01.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode.Tests
+{
+    public class TestDay01
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ AdventOfCode/Day01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    public class Day01 : BaseDay
+    {
+        private readonly string _input;
+
+        public Day01()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+
+        public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    }
+}
