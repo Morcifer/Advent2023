fae83976bcab817c1bb57cbc2aa69ebd3ca1af3b
Morcifer <mor.verbin@coolblue.nl>
2023-12-01 09:03:43 +0100

    Day 1 real: It works, but it needs cleanup and tests and an investigation of the second algorithm.

--- AdventOfCode.Tests/TestDay01.cs
+++ AdventOfCode.Tests/TestDay01.cs
@@ -2,29 +2,29 @@ namespace AdventOfCode.Tests
 {
     public class TestDay01
     {
-        [Theory]
-        [InlineData(12, 2)]
-        [InlineData(14, 2)]
-        [InlineData(1969, 654)]
-        [InlineData(100756, 33583)]
-        public void Day1_Part1_SingleRowCalculation(int mass, int expectedFuel)
-        {
-            Day01.CalculateSingleFuel(mass).Should().Be(expectedFuel);
-        }
+        //[Theory]
+        //[InlineData(12, 2)]
+        //[InlineData(14, 2)]
+        //[InlineData(1969, 654)]
+        //[InlineData(100756, 33583)]
+        //public void Day1_Part1_SingleRowCalculation(int mass, int expectedFuel)
+        //{
+        //    Day01.CalculateSingleFuel(mass).Should().Be(expectedFuel);
+        //}
+
+        //[Theory]
+        //[InlineData(12, 2)]
+        //[InlineData(14, 2)]
+        //[InlineData(1969, 966)]
+        //[InlineData(100756, 50346)]
+        //public void Day1_Part2_SingleRowCalculation(int mass, int expectedFuel)
+        //{
+        //    Day01.CalculateSingleCompoundFuel(mass).Should().Be(expectedFuel);
+        //}
 
         [Theory]
-        [InlineData(12, 2)]
-        [InlineData(14, 2)]
-        [InlineData(1969, 966)]
-        [InlineData(100756, 50346)]
-        public void Day1_Part2_SingleRowCalculation(int mass, int expectedFuel)
-        {
-            Day01.CalculateSingleCompoundFuel(mass).Should().Be(expectedFuel);
-        }
-
-        [Theory]
-        [InlineData(typeof(Day01), RunMode.Test, "34241", "51316")]
-        [InlineData(typeof(Day01), RunMode.Real, "3394032", "5088176")]
+        [InlineData(typeof(Day01), RunMode.Test, "142", "281")]
+        [InlineData(typeof(Day01), RunMode.Real, "54968", "54094")]
         public async Task Day1_Regression(Type type, RunMode runMode, string expectedPart1, string expectedPart2)
         {
             if (Activator.CreateInstance(type, runMode) is BaseTestableDay instance)
--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,11 +1,12 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("AdventOfCode.Tests")]
 namespace AdventOfCode
 {
     public sealed class Day01 : BaseTestableDay
     {
-        private readonly List<int> _input;
+        private readonly List<string> _input;
 
         public Day01() : this(RunMode.Real)
         {
@@ -17,41 +18,104 @@ namespace AdventOfCode
 
             _input = File
                 .ReadAllLines(InputFilePath)
-                .Select(int.Parse)
                 .ToList();
         }
 
-        internal static int CalculateSingleFuel(int mass)
+        internal static int CalculateSingleDigitCalibrationValue(string text)
         {
-            return (int)Math.Floor(mass / 3.0) - 2;
+            var letters = text.ToCharArray();
+
+            var first_digit = letters.First(c => char.IsDigit(c)) - '0';
+            var last_digit = letters.Last(c => char.IsDigit(c)) - '0';
+
+            return first_digit * 10 + last_digit;
         }
 
-        internal static int CalculateSingleCompoundFuel(int mass)
+        private int CalculateDigitCalibrationValue()
         {
-            int extraFuel = Math.Max(0, CalculateSingleFuel(mass));
-            int totalFuel = extraFuel;
+            return _input.Select(CalculateSingleDigitCalibrationValue).Sum();
+        }
 
-            while (extraFuel > 0)
+        //internal static int CalculateSingleLiteralCalibrationValue(string text)
+        //{
+        //    var literals = new Dictionary<string, string> ()
+        //    {
+        //        { "one", "1" },
+        //        { "two", "2" },
+        //        { "three", "3" },
+        //        { "four", "4"},
+        //        { "five", "5" },
+        //        { "six", "6"},
+        //        { "seven", "7" },
+        //        { "eight", "8" },
+        //        { "nine", "9" }
+        //    };
+
+        //    var locations_of_numbers = literals
+        //        .SelectMany(kvp => new[]
+        //        {
+        //            (text.IndexOf(kvp.Key), kvp.Key),
+        //            (text.IndexOf(kvp.Value), kvp.Value)
+        //        })
+        //        .Where(x => x.Item1 != -1)
+        //    .ToList();
+
+        //    var first = locations_of_numbers.MinBy(x => x.Item1).Item2;
+        //    var last = locations_of_numbers.MaxBy(x => x.Item1).Item2;
+
+        //    var first_digit = (literals.ContainsKey(first) ? literals[first] : first)[0] - '0';
+        //    var last_digit = (literals.ContainsKey(last) ? literals[last] : last)[0] - '0';
+
+        //    Console.WriteLine($"{text}: {first_digit} -> {last_digit}");
+
+        //    return first_digit * 10 + last_digit;
+        //}
+
+        internal static int CalculateSingleLiteralCalibrationValue(string text)
+        {
+            var literals = new Dictionary<string, string>()
             {
-                extraFuel = Math.Max(0, CalculateSingleFuel(extraFuel));
-                totalFuel += extraFuel;
+                { "one", "one1one" },
+                { "two", "two2two" },
+                { "three", "three3three" },
+                { "four", "four4four"},
+                { "five", "five5five" },
+                { "six", "six6six"},
+                { "seven", "seven7seven" },
+                { "eight", "eight8eight" },
+                { "nine", "nine9nine" }
+            };
+
+            foreach (var kvp in literals)
+            {
+                text = text.Replace(kvp.Key, kvp.Value);
             }
 
-            return totalFuel;
+            return CalculateSingleDigitCalibrationValue(text);
         }
 
-        private int CalculateFuel()
+        private int CalculateLiteralCalibrationValue()
         {
-            return _input.Select(CalculateSingleFuel).Sum();
+            var input = _input;
+            if (RunMode == RunMode.Test)
+            {
+                input = new List<string>
+                {
+                    "two1nine",
+                    "eightwothree",
+                    "abcone2threexyz",
+                    "xtwone3four",
+                    "4nineeightseven2",
+                    "zoneight234",
+                    "7pqrstsixteen",
+                };
+            }
+
+            return input.Select(CalculateSingleLiteralCalibrationValue).Sum();
         }
 
-        private int CalculateCompoundFuel()
-        {
-            return _input.Select(CalculateSingleCompoundFuel).Sum();
-        }
+        public override ValueTask<string> Solve_1() => new($"{CalculateDigitCalibrationValue()}");
 
-        public override ValueTask<string> Solve_1() => new($"{CalculateFuel()}");
-
-        public override ValueTask<string> Solve_2() => new($"{CalculateCompoundFuel()}");
+        public override ValueTask<string> Solve_2() => new($"{CalculateLiteralCalibrationValue()}");
     }
 }
