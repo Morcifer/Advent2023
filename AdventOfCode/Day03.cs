22d2ec3a498ae4f3f53866e988a787329b32c283
Morcifer <mor.verbin@coolblue.nl>
2023-12-03 11:45:01 +0100

    Day 3: A few method extractions.

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -24,6 +24,20 @@ namespace AdventOfCode
         {
             var numberLocations = new List<(int Number, int Row, List<int> Columns)>();
 
+            void MaybeUpdateNumberLocations(int rowIndex, List<int> columnIndices)
+            {
+                if (columnIndices.Any())
+                {
+                    numberLocations.Add(
+                        (
+                            int.Parse(_input[rowIndex][columnIndices[0]..(columnIndices[^1] + 1)]),
+                            rowIndex,
+                            columnIndices
+                        )
+                    );
+                }
+            }
+
             foreach (var (rowIndex, row) in _input.Enumerate())
             {
                 var foundDigits = new List<int>();
@@ -34,29 +48,15 @@ namespace AdventOfCode
                     {
                         foundDigits.Add(columnIndex);
                     }
-                    else if (foundDigits.Any())
+                    else
                     {
-                        numberLocations.Add(
-                            (
-                                int.Parse(_input[rowIndex][foundDigits[0]..(foundDigits[^1] + 1)]), 
-                                rowIndex, 
-                                foundDigits
-                            )
-                        );
+                        MaybeUpdateNumberLocations(rowIndex, foundDigits);
                         foundDigits = new List<int>();
                     }
                 }
 
-                if (foundDigits.Any())
-                {
-                    numberLocations.Add(
-                        (
-                            int.Parse(_input[rowIndex][foundDigits[0]..(foundDigits[^1] + 1)]), 
-                            rowIndex, 
-                            foundDigits
-                        )
-                    );
-                }
+                // Don't forget the last update!
+                MaybeUpdateNumberLocations(rowIndex, foundDigits);
             }
 
             return numberLocations;
@@ -64,19 +64,19 @@ namespace AdventOfCode
 
         private bool IsNextToSymbol(int row, List<int> columns)
         {
-            var neighbours = new List<(int, int)>()
+            var neighbors = new List<(int, int)>()
             {
                 (row, columns.Min() - 1),
                 (row, columns.Max() + 1),
             };
 
-            for (int i = columns.Min() - 1; i <= columns.Max() + 1; i++)
+            for (var i = columns.Min() - 1; i <= columns.Max() + 1; i++)
             {
-                neighbours.Add((row - 1, i));
-                neighbours.Add((row + 1, i));
+                neighbors.Add((row - 1, i));
+                neighbors.Add((row + 1, i));
             }
 
-            return neighbours
+            return neighbors
                 .Where(x => x.Item1 >= 0 && x.Item1 < _input[0].Length && x.Item2 >= 0 && x.Item2 < _input[0].Length)
                 .Any(x => _input[x.Item1][x.Item2] != '.' && !char.IsDigit(_input[x.Item1][x.Item2]));
         }
@@ -91,19 +91,8 @@ namespace AdventOfCode
                 .Sum();
         }
 
-        private bool IsNextToGear(int row, List<int> columns, int gearRow, int gearColumn)
+        private List<(int Row, int Column)> FindGears()
         {
-            return gearRow >= row - 1 
-                   && gearRow <= row + 1 
-                   && gearColumn >= columns.Min() - 1 
-                   && gearColumn <= columns.Max() + 1;
-        }
-
-        private Answer CalculateGearRatioSums()
-        {
-            var numberLocations = FindNumbers();
-
-            // Find all gear locations
             var gearLocations = new List<(int Row, int Column)>();
 
             foreach (var (rowIndex, row) in _input.Enumerate())
@@ -117,15 +106,30 @@ namespace AdventOfCode
                 }
             }
 
+            return gearLocations;
+        }
+
+        private bool IsNextToGear(int row, List<int> columns, int gearRow, int gearColumn)
+        {
+            return gearRow >= row - 1
+                   && gearRow <= row + 1
+                   && gearColumn >= columns.Min() - 1
+                   && gearColumn <= columns.Max() + 1;
+        }
+
+        private Answer CalculateGearRatioSums()
+        {
+            var numberLocations = FindNumbers();
+            var gearLocations = FindGears();
+
             return gearLocations
-                .Select(gear => (
-                    gear, 
+                .Select(gear => 
                     numberLocations
                         .Where(number => IsNextToGear(number.Row, number.Columns, gear.Row, gear.Column))
-                        .ToList())
-                        )
-                .Where(x => x.Item2.Count == 2)
-                .Select(x => x.Item2[0].Number * x.Item2[1].Number)
+                        .ToList()
+                    )
+                .Where(partNumbers => partNumbers.Count == 2)
+                .Select(partNumbers => partNumbers[0].Number * partNumbers[1].Number)
                 .Sum();
         }
 
