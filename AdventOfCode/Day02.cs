6ae9bd05d28247ab994feafce5799d607b359463
Wessel van Lit <wessel.vanlit@coolblue.nl>
2023-12-04 12:57:40 +0100

    [master] Set InternalsVisibleTo in .csproj instead
    Also use file-scoped namespaces

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -1,111 +1,107 @@
-﻿using System.Runtime.CompilerServices;
+﻿namespace AdventOfCode;
 
-[assembly: InternalsVisibleTo("AdventOfCode.Tests")]
-namespace AdventOfCode
+public sealed class Day02 : BaseTestableDay
 {
-    public sealed class Day02 : BaseTestableDay
+    private readonly List<Game> _input;
+
+    public class GameSession
     {
-        private readonly List<Game> _input;
+        public required int Red;
 
-        public class GameSession
-        {
-            public required int Red;
+        public required int Green;
 
-            public required int Green;
-
-            public required int Blue;
-        }
-
-        public class Game
-        {
-            public required int Id;
-
-            public required List<GameSession> Sessions;
-        }
-
-        public Day02() : this(RunMode.Real)
-        {
-        }
-
-        private Game ConvertTextToGame(string text)
-        {
-            // text = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-            var splitByColon = text.Split(':').Select(s => s.Trim()).ToList();
-            var gameId = int.Parse(splitByColon[0][5..]);
-
-            // sessions = "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-            var sessions = splitByColon[1]
-                .Split(';')
-                .Select(s => s.Trim())
-                .ToList();
-
-            var result = new List<Dictionary<string, int>>();
-
-            foreach (var session in sessions)
-            {
-                var cubes = session // session = "3 blue, 4 red"
-                    .Split(',')
-                    .Select(s => s.Trim())
-                    .Select(s => s.Split(' ')) // "3 blue" => ["3", "blue"]
-                    .ToDictionary(x => x[1], x => int.Parse(x[0]));
-
-                result.Add(cubes);
-            }
-
-            return new Game
-            {
-                Id = gameId,
-                Sessions = result
-                    .Select(
-                        session => new GameSession()
-                        {
-                            Red = session.GetValueOrDefault("red", 0),
-                            Blue = session.GetValueOrDefault("blue", 0),
-                            Green = session.GetValueOrDefault("green", 0),
-                        }
-                    )
-                    .ToList()
-            };
-        }
-
-        public Day02(RunMode runMode)
-        {
-            RunMode = runMode;
-
-            _input = File
-                .ReadAllLines(InputFilePath)
-                .Select(ConvertTextToGame)
-                .ToList();
-        }
-
-        internal static (int Red, int Green, int Blue) GetMaxRequired(Game game)
-        {
-            var maxRed = game.Sessions.Select(session => session.Red).Max();
-            var maxGreen = game.Sessions.Select(session => session.Green).Max();
-            var maxBlue = game.Sessions.Select(session => session.Blue).Max();
-
-            return (maxRed, maxGreen, maxBlue);
-        }
-
-        private Answer CalculateNumberOfPossibleGames()
-        {
-            return _input
-                .Select(game => (gameId: game.Id, max: GetMaxRequired(game)))
-                .Where(x => x.max is { Red: <= 12, Green: <= 13, Blue: <= 14 })
-                .Select(x => x.gameId)
-                .Sum();
-        }
-
-        private Answer MinimalCubeCount()
-        {
-            return _input
-                .Select(GetMaxRequired)
-                .Select(x => x.Red * x.Green * x.Blue)
-                .Sum();
-        }
-
-        public override ValueTask<string> Solve_1() => new(CalculateNumberOfPossibleGames());
-
-        public override ValueTask<string> Solve_2() => new(MinimalCubeCount());
+        public required int Blue;
     }
+
+    public class Game
+    {
+        public required int Id;
+
+        public required List<GameSession> Sessions;
+    }
+
+    public Day02() : this(RunMode.Real)
+    {
+    }
+
+    private Game ConvertTextToGame(string text)
+    {
+        // text = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+        var splitByColon = text.Split(':').Select(s => s.Trim()).ToList();
+        var gameId = int.Parse(splitByColon[0][5..]);
+
+        // sessions = "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+        var sessions = splitByColon[1]
+            .Split(';')
+            .Select(s => s.Trim())
+            .ToList();
+
+        var result = new List<Dictionary<string, int>>();
+
+        foreach (var session in sessions)
+        {
+            var cubes = session // session = "3 blue, 4 red"
+                .Split(',')
+                .Select(s => s.Trim())
+                .Select(s => s.Split(' ')) // "3 blue" => ["3", "blue"]
+                .ToDictionary(x => x[1], x => int.Parse(x[0]));
+
+            result.Add(cubes);
+        }
+
+        return new Game
+        {
+            Id = gameId,
+            Sessions = result
+                .Select(
+                    session => new GameSession()
+                    {
+                        Red = session.GetValueOrDefault("red", 0),
+                        Blue = session.GetValueOrDefault("blue", 0),
+                        Green = session.GetValueOrDefault("green", 0),
+                    }
+                )
+                .ToList()
+        };
+    }
+
+    public Day02(RunMode runMode)
+    {
+        RunMode = runMode;
+
+        _input = File
+            .ReadAllLines(InputFilePath)
+            .Select(ConvertTextToGame)
+            .ToList();
+    }
+
+    internal static (int Red, int Green, int Blue) GetMaxRequired(Game game)
+    {
+        var maxRed = game.Sessions.Select(session => session.Red).Max();
+        var maxGreen = game.Sessions.Select(session => session.Green).Max();
+        var maxBlue = game.Sessions.Select(session => session.Blue).Max();
+
+        return (maxRed, maxGreen, maxBlue);
+    }
+
+    private Answer CalculateNumberOfPossibleGames()
+    {
+        return _input
+            .Select(game => (gameId: game.Id, max: GetMaxRequired(game)))
+            .Where(x => x.max is { Red: <= 12, Green: <= 13, Blue: <= 14 })
+            .Select(x => x.gameId)
+            .Sum();
+    }
+
+    private Answer MinimalCubeCount()
+    {
+        return _input
+            .Select(GetMaxRequired)
+            .Select(x => x.Red * x.Green * x.Blue)
+            .Sum();
+    }
+
+    public override ValueTask<string> Solve_1() => new(CalculateNumberOfPossibleGames());
+
+    public override ValueTask<string> Solve_2() => new(MinimalCubeCount());
 }
--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -1,106 +1,102 @@
-﻿using System.Runtime.CompilerServices;
+﻿namespace AdventOfCode;
 
-[assembly: InternalsVisibleTo("AdventOfCode.Tests")]
-namespace AdventOfCode
+public sealed class Day04 : BaseTestableDay
 {
-    public sealed class Day04 : BaseTestableDay
+    private readonly List<Card> _input;
+
+    public class Card
     {
-        private readonly List<Card> _input;
+        public required int Id;
 
-        public class Card
+        public required HashSet<int> Winning;
+
+        public required HashSet<int> Have;
+
+        public int NumberMatches()
         {
-            public required int Id;
-
-            public required HashSet<int> Winning;
-
-            public required HashSet<int> Have;
-
-            public int NumberMatches()
-            {
-                return Winning.Intersect(Have).ToList().Count;
-            }
+            return Winning.Intersect(Have).ToList().Count;
         }
-
-        public Day04() : this(RunMode.Test)
-        {
-        }
-
-        public Day04(RunMode runMode)
-        {
-            RunMode = runMode;
-
-            _input = File
-                .ReadAllLines(InputFilePath)
-                .Select(ConvertTextToCard)
-                .ToList();
-        }
-
-
-        private Card ConvertTextToCard(string text)
-        {
-            // text = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-            var splitByColon = text.Split(':').Select(s => s.Trim()).ToList();
-            var cardId = int.Parse(splitByColon[0][5..]);
-
-            // numbers = "41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-            var numbers = splitByColon[1]
-                .Split('|')
-                .Select(s => s.Trim())
-                .ToList();
-
-            return new Card
-            {
-                Id = cardId,
-                Winning = numbers[0] // "41 48 83 86 17"
-                    .Split(' ')
-                    .Select(s => s.Trim())
-                    .Where(s => s != "")
-                    .Select(int.Parse)
-                    .ToHashSet(),
-                Have = numbers[1] // "83 86  6 31 17  9 48 53"
-                    .Split(' ')
-                    .Select(s => s.Trim())
-                    .Where(s => s != "")
-                    .Select(int.Parse)
-                    .ToHashSet(),
-            };
-        }
-
-        private Answer CalculateCardPoints()
-        {
-            return _input
-                .Select(card => card.NumberMatches())
-                .Select(matches => matches > 0 ? (int)Math.Pow(2, matches - 1) : 0)
-                .Sum();
-        }
-
-        private Answer CalculateCardNumber()
-        {
-            var cardIds = _input.Select(card => card.Id).ToHashSet();
-            var cardScores = _input.ToDictionary(card => card.Id, card => card.NumberMatches());
-            var cardAmounts = _input.ToDictionary(card => card.Id, _ => 1);
-
-            foreach (var card in _input)
-            {
-                var score = cardScores[card.Id];
-                var amount = cardAmounts[card.Id];
-
-                foreach (var cardsAhead in Enumerable.Range(1, score))
-                {
-                    if (!cardIds.Contains(card.Id + cardsAhead))
-                    {
-                        break;
-                    }
-
-                    cardAmounts[card.Id + cardsAhead] += amount;
-                }
-            }
-
-            return cardAmounts.Values.Sum();
-        }
-
-        public override ValueTask<string> Solve_1() => new(CalculateCardPoints());
-
-        public override ValueTask<string> Solve_2() => new(CalculateCardNumber());
     }
+
+    public Day04() : this(RunMode.Test)
+    {
+    }
+
+    public Day04(RunMode runMode)
+    {
+        RunMode = runMode;
+
+        _input = File
+            .ReadAllLines(InputFilePath)
+            .Select(ConvertTextToCard)
+            .ToList();
+    }
+
+
+    private Card ConvertTextToCard(string text)
+    {
+        // text = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+        var splitByColon = text.Split(':').Select(s => s.Trim()).ToList();
+        var cardId = int.Parse(splitByColon[0][5..]);
+
+        // numbers = "41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+        var numbers = splitByColon[1]
+            .Split('|')
+            .Select(s => s.Trim())
+            .ToList();
+
+        return new Card
+        {
+            Id = cardId,
+            Winning = numbers[0] // "41 48 83 86 17"
+                .Split(' ')
+                .Select(s => s.Trim())
+                .Where(s => s != "")
+                .Select(int.Parse)
+                .ToHashSet(),
+            Have = numbers[1] // "83 86  6 31 17  9 48 53"
+                .Split(' ')
+                .Select(s => s.Trim())
+                .Where(s => s != "")
+                .Select(int.Parse)
+                .ToHashSet(),
+        };
+    }
+
+    private Answer CalculateCardPoints()
+    {
+        return _input
+            .Select(card => card.NumberMatches())
+            .Select(matches => matches > 0 ? (int)Math.Pow(2, matches - 1) : 0)
+            .Sum();
+    }
+
+    private Answer CalculateCardNumber()
+    {
+        var cardIds = _input.Select(card => card.Id).ToHashSet();
+        var cardScores = _input.ToDictionary(card => card.Id, card => card.NumberMatches());
+        var cardAmounts = _input.ToDictionary(card => card.Id, _ => 1);
+
+        foreach (var card in _input)
+        {
+            var score = cardScores[card.Id];
+            var amount = cardAmounts[card.Id];
+
+            foreach (var cardsAhead in Enumerable.Range(1, score))
+            {
+                if (!cardIds.Contains(card.Id + cardsAhead))
+                {
+                    break;
+                }
+
+                cardAmounts[card.Id + cardsAhead] += amount;
+            }
+        }
+
+        return cardAmounts.Values.Sum();
+    }
+
+    public override ValueTask<string> Solve_1() => new(CalculateCardPoints());
+
+    public override ValueTask<string> Solve_2() => new(CalculateCardNumber());
 }
