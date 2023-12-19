5cb3d4191e580963510c908c2b9e2c7baf0c194c
Morcifer <mor.verbin@coolblue.nl>
2023-12-19 18:44:25 +0100

    Day 19: Good enough for government work.

--- AdventOfCode/Day19.cs
+++ AdventOfCode/Day19.cs
@@ -1,11 +1,17 @@
 namespace AdventOfCode;
 
-// TODO: Make XMAS tuple to get rid of the char.
+public enum Xmas
+{
+    X,
+    M,
+    A,
+    S,
+}
 
 public sealed class Day19 : BaseTestableDay
 {
-    private readonly Dictionary<string, List<(char Value, char Rule, int Limit, string Next)>> _rules;
-    private readonly List<Dictionary<char, int>> _parts;
+    private readonly Dictionary<string, List<(Xmas Value, char Rule, int Limit, string Next)>> _workflows;
+    private readonly List<Dictionary<Xmas, int>> _parts;
 
     public Day19() : this(RunMode.Real)
     {
@@ -20,23 +26,36 @@ public sealed class Day19 : BaseTestableDay
             .Cluster()
             .ToList();
 
-        _rules = input[0].Select(ConvertTextToRule).ToDictionary(x => x.Item1, x => x.Item2);
+        _workflows = input[0].Select(ConvertTextToWorkflow).ToDictionary(x => x.Item1, x => x.Item2);
         _parts = input[1].Select(ConvertTextToPart).ToList();
     }
 
-    private (string, List<(char Value, char Rule, int Limit, string Next)>) ConvertTextToRule(string text)
+    private Xmas ConvertCharToXmas(char value)
+    {
+        return value switch
+        {
+            'x' => Xmas.X,
+            'm' => Xmas.M,
+            'a' => Xmas.A,
+            's' => Xmas.S,
+            _ => throw new ArgumentOutOfRangeException("Xmas", "Did you mess something up?"),
+        };
+    }
+
+    private (string, List<(Xmas Value, char Rule, int Limit, string Next)>) ConvertTextToWorkflow(string text)
     {
         var split = text[..^1].Split('{');
         var name = split[0];
         var workflows = split[1].Split(',');
-        var result = new List<(char Value, char Rule, int Limit, string Next)>();
+        var result = new List<(Xmas Value, char Rule, int Limit, string Next)>();
 
         foreach (var workflow in workflows)
         {
             var semicolonLocation = workflow.IndexOf(':');
+
             result.Add(
                 (
-                    semicolonLocation == -1 ? 'x' : workflow[0],
+                    semicolonLocation == -1 ? Xmas.X : ConvertCharToXmas(workflow[0]),
                     semicolonLocation == -1 ? '>' : workflow[1],
                     semicolonLocation == -1 ? 0 : int.Parse(workflow[2..semicolonLocation]),
                     semicolonLocation == -1 ? workflow : workflow[(semicolonLocation + 1)..]
@@ -47,40 +66,44 @@ public sealed class Day19 : BaseTestableDay
         return (name, result);
     }
 
-    private Dictionary<char, int> ConvertTextToPart(string text)
+    private Dictionary<Xmas, int> ConvertTextToPart(string text)
     {
-        return text[1..^1].Split(',').Select(x => x.Split('=')).ToDictionary(x => x[0][0], x => int.Parse(x[1]));
+        return text[1..^1]
+            .Split(',')
+            .Select(x => x.Split('='))
+            .ToDictionary(x => ConvertCharToXmas(x[0][0]), x => int.Parse(x[1]));
     }
 
-    private bool DoesRuleApply(Dictionary<char, int> part, (char Value, char Rule, int Limit, string Next) rule)
+    private bool DoesRuleApply(Dictionary<Xmas, int> part, (Xmas Value, char Rule, int Limit, string Next) rule)
     {
         var relevantValue = part[rule.Value];
+
         return rule.Rule switch
         {
             '>' => relevantValue > rule.Limit,
             '<' => relevantValue < rule.Limit,
-            _ => throw new ArgumentOutOfRangeException("Did you mess something up?"),
+            _ => throw new ArgumentOutOfRangeException("Rule", "Did you mess something up?"),
         };
     }
 
-    private int CalculatePartRating(Dictionary<char, int> part)
+    private int CalculatePartRating(Dictionary<Xmas, int> part)
     {
         //Console.WriteLine($"Evaluating Part");
-        var workflow = "in";
+        var workflowName = "in";
 
-        while (workflow != "R" && workflow != "A")
+        while (workflowName != "R" && workflowName != "A")
         {
             //Console.WriteLine($"Am in workflow {workflow}");
-            var rule = _rules[workflow];
-            workflow = rule.First(x => DoesRuleApply(part, x)).Next;
+            var workflow = _workflows[workflowName];
+            workflowName = workflow.First(x => DoesRuleApply(part, x)).Next;
         }
 
         //Console.WriteLine($"Am in workflow {workflow}");
-        return workflow switch
+        return workflowName switch
         {
             "A" => part.Values.Sum(),
             "R" => 0,
-            _ => throw new ArgumentOutOfRangeException("Did you mess something up?"),
+            _ => throw new ArgumentOutOfRangeException("workflow", "Did you mess something up?"),
         };
     }
 
@@ -89,50 +112,37 @@ public sealed class Day19 : BaseTestableDay
         return _parts.Select(CalculatePartRating).Sum();
     }
 
-    private List<Dictionary<char, (int Start, int End)>> ApplyRuleOnRange(Dictionary<char, (int Start, int End)> range, string workflow)
+    private List<Dictionary<Xmas, (int Start, int End)>> ApplyRuleOnRange(Dictionary<Xmas, (int Start, int End)> range, string workflowName)
     {
-        var acceptedRanges = new List<Dictionary<char, (int Start, int End)>>();
+        var acceptedRanges = new List<Dictionary<Xmas, (int Start, int End)>>();
 
         // Make splits based on this workflow. Then iteratively extend for each split and take out the accepted.
         var currentRange = range;
-        var rule = _rules[workflow];
+        var workflow = _workflows[workflowName];
 
-        foreach (var subrule in rule)
+        foreach (var rule in workflow)
         {
             // Split the range into two, the one that stays and the one that continues.
-            var relevantRange = currentRange[subrule.Value];
+            var relevantRange = currentRange[rule.Value];
 
-            // TODO: Combine these into one statement.
-            var rangeThatApplies = subrule.Rule switch
+            var (rangeThatApplies, rangeThatContinues) = rule.Rule switch
             {
-                '>' when relevantRange.Start > subrule.Limit => relevantRange,
-                '>' when relevantRange.End <= subrule.Limit => (0, 0),
-                '>' => (subrule.Limit + 1, relevantRange.End),
-                '<' when relevantRange.End < subrule.Limit => relevantRange,
-                '<' when relevantRange.Start >= subrule.Limit => (0, 0),
-                '<' => (relevantRange.Start, subrule.Limit - 1),
-                _ => throw new ArgumentOutOfRangeException("Did you mess something up?"),
+                '>' when relevantRange.Start > rule.Limit => (relevantRange, (0, 0)),
+                '>' when relevantRange.End <= rule.Limit => ((0, 0), relevantRange),
+                '>' => ((rule.Limit + 1, relevantRange.End), (relevantRange.Start, rule.Limit)),
+                '<' when relevantRange.End < rule.Limit => (relevantRange, (0, 0)),
+                '<' when relevantRange.Start >= rule.Limit => ((0, 0), relevantRange),
+                '<' => ((relevantRange.Start, rule.Limit - 1), (rule.Limit, relevantRange.End)),
+                _ => throw new ArgumentOutOfRangeException("rule", "Did you mess something up?"),
             };
 
-            var fullRangeThatApplies = currentRange.ToDictionary(kvp => kvp.Key, kvp => kvp.Key == subrule.Value ? rangeThatApplies : kvp.Value);
-
-            var rangeThatContinues = subrule.Rule switch
-            {
-                '>' when relevantRange.Start > subrule.Limit => (0, 0),
-                '>' when relevantRange.End <= subrule.Limit => relevantRange,
-                '>' => (relevantRange.Start, subrule.Limit),
-                '<' when relevantRange.End < subrule.Limit => (0, 0),
-                '<' when relevantRange.Start >= subrule.Limit => relevantRange,
-                '<' => (subrule.Limit, relevantRange.End),
-                _ => throw new ArgumentOutOfRangeException("Did you mess something up?"),
-            };
-
-            var fullRangeThatContinues = currentRange.ToDictionary(kvp => kvp.Key, kvp => kvp.Key == subrule.Value ? rangeThatContinues : kvp.Value);
+            var fullRangeThatApplies = currentRange.ToDictionary(kvp => kvp.Key, kvp => kvp.Key == rule.Value ? rangeThatApplies : kvp.Value);
+            var fullRangeThatContinues = currentRange.ToDictionary(kvp => kvp.Key, kvp => kvp.Key == rule.Value ? rangeThatContinues : kvp.Value);
 
             // If the range still has some stuff in it!
             if (rangeThatApplies.Item2 > rangeThatApplies.Item1)
             {
-                switch (subrule.Next)
+                switch (rule.Next)
                 {
                     case "A":
                         acceptedRanges.Add(fullRangeThatApplies);
@@ -140,19 +150,17 @@ public sealed class Day19 : BaseTestableDay
                     case "R":
                         break;
                     default:
-                        acceptedRanges.AddRange(ApplyRuleOnRange(fullRangeThatApplies, subrule.Next));
+                        acceptedRanges.AddRange(ApplyRuleOnRange(fullRangeThatApplies, rule.Next));
                         break;
-                };
+                }
             }
 
-            if (rangeThatContinues.Item2 > rangeThatContinues.Item1)
-            {
-                currentRange = fullRangeThatContinues;
-            }
-            else
+            if (rangeThatContinues.Item2 <= rangeThatContinues.Item1)
             {
                 break;
             }
+
+            currentRange = fullRangeThatContinues;
         }
 
         return acceptedRanges;
@@ -160,45 +168,32 @@ public sealed class Day19 : BaseTestableDay
 
     private Answer CalculatePart2Answer()
     {
-        var startingRange = new Dictionary<char, (int Start, int End)>()
+        var startingRange = new Dictionary<Xmas, (int Start, int End)>()
         {
-            { 'x', (1, 4000) },
-            { 'm', (1, 4000) },
-            { 'a', (1, 4000) },
-            { 's', (1, 4000) },
+            { Xmas.X, (1, 4000) }, { Xmas.M, (1, 4000) }, { Xmas.A, (1, 4000) }, { Xmas.S, (1, 4000) },
         };
 
         var acceptedRanges = ApplyRuleOnRange(startingRange, "in");
 
-        //foreach (var acceptedRange in acceptedRanges)
-        //{
-        //    Console.WriteLine($"Accepted range:");
-        //    foreach (var kvp in acceptedRange)
-        //    {
-        //        Console.WriteLine($"{kvp.Key} from {kvp.Value.Start} to {kvp.Value.End}");
-        //    }
-        //}
-
         // Let's see if any of them overlap:
         foreach (var (index1, acceptedRange1) in acceptedRanges.Enumerate())
         {
-            foreach (var acceptedRange2 in acceptedRanges.Skip(index1+1))
+            foreach (var acceptedRange2 in acceptedRanges.Skip(index1 + 1))
             {
-                if (Overlaps(acceptedRange1['x'], acceptedRange2['x'])
-                    && Overlaps(acceptedRange1['m'], acceptedRange2['m'])
-                    && Overlaps(acceptedRange1['a'], acceptedRange2['a'])
-                    && Overlaps(acceptedRange1['s'], acceptedRange2['s'])
-                   )
+                if (acceptedRange1.All(kvp1 => Overlaps(kvp1.Value, acceptedRange2[kvp1.Key])))
                 {
-                    Console.WriteLine($"The following ranges overlap:");
+                    Console.WriteLine("These two ranges overlap:");
+
                     foreach (var kvp in acceptedRange1)
                     {
                         Console.WriteLine($"{kvp.Key} from {kvp.Value.Start} to {kvp.Value.End}");
                     }
+
                     foreach (var kvp in acceptedRange2)
                     {
                         Console.WriteLine($"{kvp.Key} from {kvp.Value.Start} to {kvp.Value.End}");
                     }
+
                     throw new NotImplementedException("If you're here, you better implement something for it!");
                 }
             }
@@ -210,7 +205,6 @@ public sealed class Day19 : BaseTestableDay
 
     private bool Overlaps((int Start, int End) range1, (int Start, int End) range2)
     {
-        var temp = range1.Start <= range2.End && range2.Start <= range1.End;
         return range1.Start <= range2.End && range2.Start <= range1.End;
     }
 
